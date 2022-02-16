56e58c804b1ec224d3496bc96b1f753c28d6d794
Doruk <dorukyogscast@gmail.com>
2022-02-17 00:20:27 +0300

    more comments and a recompile

--- fxdeviantart.js
+++ fxdeviantart.js
@@ -1,1 +1,54 @@
-"use strict";var __awaiter=this&&this.__awaiter||function(thisArg,_arguments,P,generator){return new(P=P||Promise)(function(resolve,reject){function fulfilled(value){try{step(generator.next(value))}catch(e){reject(e)}}function rejected(value){try{step(generator.throw(value))}catch(e){reject(e)}}function step(result){var value;result.done?resolve(result.value):((value=result.value)instanceof P?value:new P(function(resolve){resolve(value)})).then(fulfilled,rejected)}step((generator=generator.apply(thisArg,_arguments||[])).next())})},__importDefault=this&&this.__importDefault||function(mod){return mod&&mod.__esModule?mod:{default:mod}};Object.defineProperty(exports,"__esModule",{value:!0});const fastify_1=__importDefault(require("fastify")),nunjucks_1=require("nunjucks"),undici_1=require("undici"),app=(0,fastify_1.default)(),ip="0.0.0.0",PORT=process.env.port||"8080",start=()=>__awaiter(void 0,void 0,void 0,function*(){try{yield app.listen(PORT,"0.0.0.0",()=>console.log("SERVER LISTENING AT PORT : "+PORT))}catch(err){app.log.error(err),process.exit(1)}});function getSubpath(reqPar){return reqPar["*"]}function printc(text){console.log("[35;1m"+text+"[0m")}start(),app.get("/*",(req,res)=>__awaiter(void 0,void 0,void 0,function*(){var subpath=getSubpath(req.params);if(null!=subpath){subpath="https://deviantart.com/"+subpath;const body=(yield(0,undici_1.request)("https://backend.deviantart.com/oembed?url="+subpath))["body"];try{var data=yield body.json();printc(`Rendering "${subpath}"`),res.send((0,nunjucks_1.render)("templates/index.html",{img:data.url,url:subpath,desc:data.title}))}catch(error){res.send("Error Failed to Retrive Image")}}else res.send("Error No Url Provided")}));
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//Modules
+const fastify_1 = __importDefault(require("fastify")); //the REST API library
+const nunjucks_1 = require("nunjucks"); //renders the templates
+const undici_1 = require("undici"); //http request library
+const app = (0, fastify_1.default)(), //applet
+ip = "0.0.0.0", //leave like this if it works
+PORT = 8080; //change as you wish
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield app.listen(PORT, '0.0.0.0', () => console.log('SERVER LISTENING AT PORT: ' + PORT)); //server is up!
+    }
+    catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+});
+start(); //starts the server
+function getSubpath(reqPar) {
+    return reqPar["*"];
+}
+function printc(text) { console.log("\x1b[35;1m" + text + "\x1b[0m"); }
+; //cool print
+app.get('/*', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const subpath = getSubpath(req.params); //gets subpath parameter
+    if (subpath != undefined) { //check if parameter is not given
+        const origin = "https://deviantart.com/" + subpath;
+        const { body } = yield (0, undici_1.request)("https://backend.deviantart.com/oembed?url=" + origin); //http request to retrieve json
+        try { //this is basically for if body is not json OR http request returns nothing significant
+            const data = yield body.json(); //jsonify
+            printc(`Rendering "${origin}"`); //tell server manager that we are rendering something
+            res.send((0, nunjucks_1.render)('templates/index.html', { img: data['url'], url: origin, desc: data['title'] })); //return the the template
+        }
+        catch (error) { //error can be used if wanted
+            res.send("Error Failed to Retrive Image"); //these can be changed to error templates as well, if they don't work
+        }
+    }
+    else {
+        res.send("Error No Url Provided");
+    }
+}));
--- src/fxdeviantart.ts
+++ src/fxdeviantart.ts
@@ -1,15 +1,15 @@
-import fastify, { FastifyReply, FastifyRequest } from 'fastify';
-import { render } from "nunjucks";
-import { request } from "undici";
+//Modules
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'; //the REST API library
+import { render } from "nunjucks"; //renders the templates
+import { request } from "undici"; //http request library
 
-
-const app = fastify(),
+const app = fastify(), //applet
     ip: string = "0.0.0.0", //leave like this if it works
-    PORT: string = process.env.port || "8080"; //change at your disposal 
+    PORT: number = 8080; //change as you wish
 
-const start: Function = async () => {
+const start: Function = async () => { //function that will start the server
     try {
-        await app.listen(PORT, '0.0.0.0', (): void => console.log('SERVER LISTENING AT PORT : ' + PORT)) //server is up!
+        await app.listen(PORT, '0.0.0.0', (): void => console.log('SERVER LISTENING AT PORT: ' + PORT)) //server is up!
     } catch (err) {
         app.log.error(err)
         process.exit(1)
@@ -18,24 +18,24 @@ const start: Function = async () => {
 
 start(); //starts the server
 
-function getSubpath(reqPar: any): string {
+function getSubpath(reqPar: any): string { //retrieves parameters
     return reqPar["*"];
 }
-function printc(text: string): void { console.log("\x1b[35;1m" + text + "\x1b[0m") };
+
+function printc(text: string): void { console.log("\x1b[35;1m" + text + "\x1b[0m") }; //cool print
 
 app.get('/*', async (req: FastifyRequest, res: FastifyReply) => {
-    const subpath = getSubpath(req.params);
-    if (subpath != undefined) {
+    const subpath = getSubpath(req.params); //gets subpath parameter
+    if (subpath != undefined) { //check if parameter is not given
         const origin = "https://deviantart.com/" + subpath;
-        const { body } = await request("https://backend.deviantart.com/oembed?url=" + origin);
-        try {
-            const data = await body.json();
-            printc(`Rendering "${origin}"`);
-            res.send(render('templates/index.html', { img: data['url'], url: origin, desc: data['title'] }))
-        } catch (error) {
-            res.send("Error Failed to Retrive Image");
+        const { body } = await request("https://backend.deviantart.com/oembed?url=" + origin); //http request to retrieve json
+        try { //this is basically for if body is not json OR http request returns nothing significant
+            const data = await body.json(); //jsonify
+            printc(`Rendering "${origin}"`); //tell server manager that we are rendering something
+            res.send(render('templates/index.html', { img: data['url'], url: origin, desc: data['title'] })); //return the the template
+        } catch (error) { //error can be used if wanted
+            res.send("Error Failed to Retrive Image"); //these can be changed to error templates as well, if they don't work
         }
-
     } else {
         res.send("Error No Url Provided");
     }
